f402c2888556355241b2037f0153fb326c9a6a92
Asventi <jarnacpierreoff@gmail.com>
2023-11-13 14:17:31 +0100

    feat: Board Selector fini
    Selectionnez avec les fleches, bande de debiles
    
    [skip ci]

--- damesiu/board_selector.py
+++ damesiu/board_selector.py
@@ -20,19 +20,29 @@ class BoardSelector:
         main = Thread(target=self._run)
         main.start()
 
-    # TODO: Board selector a finir.
+
     def _run(self):
         self._select(self._current_cell)
         while self.graphic_engine.key != 113:
-            if self.graphic_engine.key == curses.KEY_RIGHT:
-                self._select(self.board_controller.board[0][self._current_cell.x + 1])
+            if self.graphic_engine.key == curses.KEY_UP and self._current_cell.y - 1 >= 0:
+                self._select(self.board_controller.board[self._current_cell.y - 1][self._current_cell.x])
                 # On evite la duplication de touches
                 sleep(0.1)
 
+            elif self.graphic_engine.key == curses.KEY_LEFT and self._current_cell.x - 1 >= 0:
+                self._select(self.board_controller.board[self._current_cell.y][self._current_cell.x - 1])
+                sleep(0.1)
+
+            elif self.graphic_engine.key == curses.KEY_DOWN and self._current_cell.y + 1 < self.board_controller.size:
+                self._select(self.board_controller.board[self._current_cell.y + 1][self._current_cell.x])
+                sleep(0.1)
+
+            elif self.graphic_engine.key == curses.KEY_RIGHT and self._current_cell.x + 1 < self.board_controller.size:
+                self._select(self.board_controller.board[self._current_cell.y][self._current_cell.x + 1])
+                sleep(0.1)
 
     def _select(self, cell):
         self._current_cell.selected = False
         cell.selected = True
         self._current_cell = cell
         self.graphic_engine.draw_board(self.board_controller)
-
