1c75eabbdd1812ca251da49842d5f2aab4739f43
Ilukynn <maxgau38210@gmail.com>
2023-11-29 16:21:57 +0100

    re evaluation

--- damesiu/evaluation.py
+++ damesiu/evaluation.py
@@ -18,3 +18,12 @@ def score_position(board: BoardController, joueur_code):
                 score -= 3
     return score
 
+def evaluer_position_pion(pion: Pion):
+    playable_cells = pion.get_playable_cells()
+    position_score = len(playable_cells)
+    return position_score
+
+def sauts_dispo(pion: Pion, board: BoardController):
+    playable_cells = pion.get_playable_cells()
+    sauts_disponibles = any(playable_cells)
+    return sauts_disponibles
